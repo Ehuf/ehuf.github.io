ce631aecf0d1c641f1fe3ebada4815e97e0bbcde
Mark Shaw <mark.shaw@MG641.local>
2018-12-04 15:07:38 +0000

    moved public files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <code>hello.</code> <br />
+            <code>this website is under construction.</code> <br />
+            <code>come back soon! :)</code>
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
