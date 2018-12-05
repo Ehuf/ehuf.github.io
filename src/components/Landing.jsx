67fd4012c5f63e46e79f4816f1b57d6dae882ac1
Mark Shaw <mark.shaw@MG641.local>
2018-12-05 17:56:52 +0000

    Added landing text!

---
+++ src/components/Landing.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import "./landing.scss";
+
+class Landing extends Component {
+  state = {};
+  render() {
+    return (
+        <div>
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+
+      <ul className="Words">
+        <li className="Words-line">
+          <p>&nbsp;</p>
+          <p style={{color: '#ff3244'}}>Mark Shaw</p>
+        </li>
+
+        <li className="Words-line">
+          <p>Mark Shaw</p>
+          <p>Software</p>
+        </li>
+
+        <li className="Words-line">
+          <p>Software</p>
+          <p>Developer</p>
+        </li>
+
+        <li className="Words-line">
+          <p>Developer</p>
+          <p style={{color: '#ff3244'}}>AND</p>
+        </li>
+
+        <li className="Words-line">
+          <p>AND</p>
+          <p>Chief</p>
+        </li>
+
+        <li className="Words-line">
+          <p>Chief</p>
+          <p>Saxophonist</p>
+        </li>
+
+        <li className="Words-line">
+          <p>Saxophonist</p>
+          <p>&nbsp;</p>
+        </li>
+      </ul>
+      </div>
+    );
+  }
+}
+
+export default Landing;
