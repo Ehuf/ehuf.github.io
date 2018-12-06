92be79b3ae5de042b0f4a1f3a1cf42cae5750988
Mark Shaw <mark.shaw@MG641.local>
2018-12-06 17:24:21 +0000

    Updates

---
+++ precache-manifest.8ef96fd645c63c6e7b39d981baf21fb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12ff6ecd8675efe61047",
+    "url": "/static/js/main.12ff6ecd.chunk.js"
+  },
+  {
+    "revision": "66b56df8329a7742ca8f",
+    "url": "/static/js/1.66b56df8.chunk.js"
+  },
+  {
+    "revision": "12ff6ecd8675efe61047",
+    "url": "/static/css/main.ddeda55f.chunk.css"
+  },
+  {
+    "revision": "426155b1c02a602afb1ada2de335b43f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.651260b9f3ee2f9a7241ac859829225f.js"
+  "/precache-manifest.8ef96fd645c63c6e7b39d981baf21fb8.js"
 );
 
 workbox.clientsClaim();
