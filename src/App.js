6d24d52eac1b7f13b5affe24885fedd936df343a
Mark Shaw <mark.shaw@MG641.local>
2018-12-06 17:23:35 +0000

    maybe fix

--- src/App.js
+++ src/App.js
@@ -3,19 +3,90 @@ import "./App.css";
 import Navigation from "./components/Navigation";
 import Landing from "./components/Landing";
 import Particles from "react-particles-js";
+import "./components/particles.css"
 
 const bgParams = {
   particles: {
     number: {
-      value: 150,
-      size: 200,
+      value: 300,
       density: {
         enable: true,
-        value_area: 800
+        value_area: 1000
       }
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "edge",
+      stroke: {
+        width: 0,
+        color: "#000000"
+      },
+      polygon: {
+        nb_sides: 5
+      },
+
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 100,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: false,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "repulse"
+      },
+      onclick: {
+        enable: false,
+        mode: "push"
+      },
+      resize: true
     }
   }
 }
+
+
 
 
 class App extends Component {
